56cfc5bce761bfbfe8d298ef085407fe559ea108
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-07-27 13:12:00 +0100

    adding api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappc51c5304": {
       "AppClientID": "string",
